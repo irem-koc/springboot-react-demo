a0b9a641dc1ab613509ab6d51c2e34e61b5dea8f
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-11-29 22:27:57 +0300

    Add Navbar component with basic structure and styling

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <ul className="p-5 bg-green-500">
+      <li>Öğrenci Listesi</li>
+      <li>Öğrenci Ekle</li>
+    </ul>
+  );
+};
+
+export default Navbar;
