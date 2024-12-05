f01bd6cea0133f668e0c86def6bd60f7ce32f1f5
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-05 21:57:53 +0300

    Add PageHeader and StudentListItem components to Home page

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,11 +1,20 @@
+import PageHeader from "../../components/PageHeader";
+import StudentListItem from "../../components/StudentListItem";
 import { useApiGetStudents } from "../../hooks/api";
 
-type Props = {};
-
-const Home = (props: Props) => {
+const Home = () => {
   const { data, isLoading } = useApiGetStudents();
   console.log(data, isLoading);
-  return <div>Home</div>;
+  return (
+    <div className="mt-2">
+      <PageHeader title="Öğrenci Listesi" />
+
+      {data &&
+        data?.map((studentItem: Student) => (
+          <StudentListItem key={studentItem.id} student={studentItem} />
+        ))}
+    </div>
+  );
 };
 
 export default Home;
